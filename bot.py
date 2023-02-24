3b24b1693d887d33b23c9227d81cac725f3187fc
emhedge <122066271+emhedge@users.noreply.github.com>
2023-02-25 01:38:56 +0800

    Well, this might work now.

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import responses
 
+
 async def send_message(message, user_message, is_private):
     try:
         response = responses.get_response(user_message)
@@ -10,14 +11,14 @@ async def send_message(message, user_message, is_private):
         print(e)
 
 def run_discord_bot():
-    TOKEN = 'MTA3MjUwODgyNTc3NDYwNDQxOQ.G1a2Uj.CFeX3ecw81ttdsARpaASblBWd8kj6Zj_umqBPI'
+    TOKEN = 'MTA3MjUwODgyNTc3NDYwNDQxOQ.GnV5x1.A8prxdtfiP2rkdlxnDBnZZ7kkrd9oXQtQnmJEc'
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
 
     @client.event
     async def on_ready():
-            print(f'{client.user} is now running!')
+        print(f'{client.user} is now running!')
 
     @client.event
     async def on_message(message):
@@ -27,7 +28,7 @@ def run_discord_bot():
         user_message = str(message.content)
         channel = str(message.channel)
 
-        print(f'{username} said "{user_message}" ({channel})')
+        print(f'{username} said: "{user_message}" ({channel})')
 
         if user_message[0] == '?':
             user_message = user_message[1:]
--- responses.py
+++ responses.py
@@ -2,26 +2,21 @@ import random
 
 def get_response(message: str) -> str:
     p_message = message.lower()
-
     if p_message == 'hello':
-            return '*grunt*'
-
-    if p_message == 'roll':
+        return 'Hey there!'
+    if message == 'roll':
         return str(random.randint(1, 6))
-
-    if p_message == '!help':
-        return "`If you come to a witcher for help, you're truly fucked.`"
-
     if p_message == 'flip':
         return str(random.choice(['Heads', 'Tails']))
-
     if p_message == 'where to?':
-        return str(random.choice(['Lonely Labs', 'Breakwater Bay', 'Brutal Bastion', 'Faulty Splits', 'Frenzy Fields', 'Shattered Slabs', 'Slappy Shores', 'The Citadel', 'Anvil Square', 'Somewhere cold', 'Somewhere wet', 'A hot zone', 'Somewhere quiet']))
+        return str(random.choice(['Lonely Labs', 'Breakwater Bay', 'Brutal Bastion', 'Faulty Splits', 'Frenzy Fields', 'Shattered Slabs', 'Slappy Shores', 'The Citadel', 'Anvil Square', 'Somewhere cold', 'Somewhere wet', 'A hot zone', 'Somewhere quiet', 'middle of fuckin\' nowhere', 'somewhere gay for John']))
+    # not finished if p_message == 'Tell me something interesting':
+        # need to figure it out return str(random.choice[''])
+    if p_message == 'berate me':
+        return str(random.choice(['f']))
+    if p_message == '!help':
+        return '`Witchers help those who help themselves.`'
 
-    if p_message == 'Tell me something interesting':
-        return str(random.choice[''])
-
-    return 'huh?'
 
 
 
